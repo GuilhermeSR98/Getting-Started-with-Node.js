3cdbbf7aba27d8f417c2d2b367d66c5b2200e612
GuilhermeSR98 <guigareisbr@gmail.com>
2021-07-10 12:04:18 -0300

    :sparkle:Using EXPRESS

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
-const http = require("http");
+const express = require("express");
+const server = express();
 
-const server = function (port) {
-  http
-    .createServer(function (req, res) {
-      res.end("Hello");
-    })
-    .listen(port);
-  console.log("Server On");
-};
+const initServer = function(port){
+  server.listen(port, function(){
+    console.log(`Server ON >> http://localhost:${port}`)
+  })
+}
 
-module.exports = server;
+module.exports = initServer;
+
+//const http = require("http");
+
+//const server = function (port) {
+//  http
+//    .createServer(function (req, res) {
+//      res.end("Hello");
+//    })
+//    .listen(port);
+//  console.log("Server On");
+//};
+
+//module.exports = server;
