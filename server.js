a62d098681c934349d3969c74cf92e8f0a439d47
GuilhermeSR98 <guigareisbr@gmail.com>
2021-07-10 15:57:03 -0300

    :hammer: Organizing routes

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const express = require("express");
 const server = express();
 
+server.get("/", function (req, res) {
+  res.sendFile(__dirname + '/html/index.html');
+});
+
+server.get("/about", function (req, res) {
+  res.send("Welcome to the about page!");
+});
+
+server.get("/blog", function (req, res) {
+  res.send("Welcome to the blog page!");
+});
+
+server.get("/:nome/", function (req, res) {
+  res.send(`Welcome ${req.params.nome}`);
+});
+
 const initServer = function(port){
   server.listen(port, function(){
     console.log(`Server ON >> http://localhost:${port}`)
